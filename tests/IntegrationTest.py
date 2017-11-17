3410149bfd8fc944dc82bce959d32ef4706b590f
wtjerry <wtjerry123@gmail.com>
2017-11-17 13:30:42 +0100

    added IntegrationTest.

---
+++ tests/IntegrationTest.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest import mock
+from unittest.mock import MagicMock
+
+from prenNetworkConnection.ConnectionHandler import ConnectionHandler
+
+
+class IntegrationTest(unittest.TestCase):
+    def test_connection_handling(self):
+        with mock.patch("prenNetworkConnection.StartCommand.StartCommand.execute") as start_signal_mock:
+            connection_mock = MagicMock()
+            test_data = b"000011"
+            connection_mock.recv.return_value = test_data
+
+            ConnectionHandler().handle(connection_mock, "client address")
+
+            self.assertTrue(start_signal_mock.called)
