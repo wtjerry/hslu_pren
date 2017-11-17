5f4637fd55eed7b8b25b43a77775869e9a1f03ad
wtjerry <wtjerry123@gmail.com>
2017-11-17 16:08:52 +0100

    added Encoder.

---
+++ tests/EncoderTest.py
@@ -0,0 +1,10 @@
+import unittest
+
+from prenNetworkConnection.Encoder import Encoder
+
+
+class EncoderTest(unittest.TestCase):
+    def test_encode_position(self):
+        self.assertEqual(b"00003225", Encoder().position(2, 5))
+        self.assertEqual(b"00003291", Encoder().position(9, 1))
+        self.assertEqual(b"0000524455", Encoder().position(44, 55))
