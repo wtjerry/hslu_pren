59898e61e99abec87c88d99750c4f574fb2e0450
wtjerry <wtjerry123@gmail.com>
2017-11-24 12:12:48 +0100

    modified pren.service file to point to correct deployment folder.

--- prenNetworkConnection/IpProvider.py
+++ prenNetworkConnection/IpProvider.py
@@ -5,12 +5,10 @@ import struct
 
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(
-        fcntl.ioctl(
-            s.fileno(),
-            0x8915,
-            struct.pack('256s', bytes(ifname[:15], 'utf-8'))
-        )[20:24])
+    p = struct.pack('256s', bytes(ifname[:15], 'utf-8'))
+    ioctl = fcntl.ioctl(s.fileno(), 0x8915, p)
+    ioctl_ = ioctl[20:24]
+    return socket.inet_ntoa(ioctl_)
 
 
 def get_wlan_ip_address():
