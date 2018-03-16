1095b4020b03dbb469513a259415f2b6349ccf15
wtjerry <wtjerry123@gmail.com>
2018-03-16 14:50:15 +0100

    cleanup naming of private members in Controller

--- controlling/Binding.py
+++ controlling/Binding.py
@@ -19,18 +19,15 @@ class Binding:
     use_real_telescope = False
     use_real_x_position = False
 
-    def __init__(self, goalfound_function):
+    def __init__(self, goal_found_function):
         # TODO replace Dummy(), it raises an error when initialized
         self.movement = Dummy() if self.use_real_movement else DummyMovement()
         self.x_position = Dummy() if self.use_real_x_position else DummyXPosition(self.movement)
         self.balancer = Dummy() if self.use_real_balancer else DummyBalanceEngine(self.x_position)
-        self.target_detection = Dummy() if self.use_real_goal_detection else DummyTargetDetection(goalfound_function)
+        self.target_detection = Dummy() if self.use_real_goal_detection else DummyTargetDetection(goal_found_function)
         self.load_position_comparer = Dummy() if self.use_real_load_position_comparer \
             else DummyLoadPositionComparer(self.x_position)
-
         self.magnet = self._get_real_magnet() if self.use_real_magnet else DummyMagnet()
-        # self.position_calculator = Dummy() if self.use_real_position_calculator else DummyPositionCalculator()
-        # self.position_output = Dummy() if self.use_real_position_output else DummyPositionOutput()
         self.telescope = Dummy() if self.use_real_telescope else DummyTelescope()
 
     def _get_real_magnet(self):
--- controlling/Controller.py
+++ controlling/Controller.py
@@ -11,76 +11,70 @@ from networking.SocketServer import SocketServer
 
 class Controller(object):
     def __init__(self):
-        self.executor = ThreadPoolExecutor(max_workers=4)
+        self._executor = ThreadPoolExecutor(max_workers=4)
+
         binding = Binding(self.on_target_found)
-        self.target_detection = binding.target_detection
+        self._target_detection = binding.target_detection
         self._movement = binding.movement
-        self.x_position = binding.x_position
+        self._x_position = binding.x_position
         self._balancer = binding.balancer
-        self.load_position_comparer = binding.load_position_comparer
-        self.telescope = binding.telescope
-        self.magnet = binding.magnet
+        self._load_position_comparer = binding.load_position_comparer
+        self._telescope = binding.telescope
+        self._magnet = binding.magnet
+
         ip = get_wlan_ip_address()
-        handler = ConnectionHandler()
-        self._socket_server = SocketServer(handler, address=ip)
-        self._logger = Logger(handler)
-        self._position_sender = PositionSender(handler)
+        connection_handler = ConnectionHandler()
+        self._socket_server = SocketServer(connection_handler, address=ip)
+        self._logger = Logger(connection_handler)
+        self._position_sender = PositionSender(connection_handler)
 
     def listen_for_start(self):
-        print("----------------------------")
-        print("Listening for start")
-        print("----------------------------")
-        print("                            ")
         self._socket_server.start(self._enqueue_on_start)
 
     def _enqueue_on_start(self):
-        self.executor.submit(self._on_start)
+        self._executor.submit(self._on_start)
 
     def _on_start(self):
-        print("----------------------------")
-        print("switching to start now..")
-        print("----------------------------")
-        print("                            ")
-        self.executor.submit(self._balancer.start)
+        self._logger.major_step("Switching to start")
+        self._executor.submit(self._balancer.start)
         self._move_until_load_reached()
 
     def _move_until_load_reached(self):
-        self._logger.major_step("Move until load reached")
+        self._logger.major_step("Moving to load")
         self._position_sender.send(42, 1337)
         self._movement.start_moving()
-        self.load_position_comparer.check_until_reached()
+        self._load_position_comparer.check_until_reached()
         self._movement.stop_moving()
         self._get_load()
 
     def _get_load(self):
-        self._logger.major_step("getting the load")
-        self.telescope.down(100)
-        self.magnet.start()
+        self._logger.major_step("Getting the load")
+        self._telescope.down(100)
+        self._magnet.start()
         time.sleep(1)
-        self.telescope.up(100)
+        self._telescope.up(100)
         self._move_until_target_reached()
 
     def _move_until_target_reached(self):
-        self._logger.major_step("move until target reached")
+        self._logger.major_step("Moving to target")
         self._movement.start_moving()
-        self.executor.submit(self.target_detection.start)
+        self._executor.submit(self._target_detection.start)
 
     def on_target_found(self):
-        self._logger.major_step("Target found!")
+        self._logger.major_step("Target found")
         time.sleep(0.3)
         self._movement.stop_moving()
         self._deliver_load()
 
     def _deliver_load(self):
-        self._logger.major_step("Deliver Load")
-        self.telescope.down(150)
-        self.magnet.stop()
+        self._logger.major_step("Delivering Load")
+        self._telescope.down(150)
+        self._magnet.stop()
         time.sleep(0.5)
-        print("move until finnished")
         self._movement.start_moving()
         time.sleep(1)
         self._movement.stop_moving()
         self._balancer.stop()
-        self._logger.major_step("finished")
+        self._logger.major_step("Finished")
         time.sleep(0.5)
         self._socket_server.stop()
