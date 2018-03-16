22298f7e9f491d0026c73227b5abfbca472abb4c
Samse <samuelberisha@gmail.com>
2018-03-16 09:06:45 +0100

    Defined the position class

--- position/Position.py
+++ position/Position.py
@@ -1,4 +1,6 @@
 import time
+import math
+
 from position.XPositionSensor import XPositionSensor
 
 
@@ -7,29 +9,30 @@ class Position(object):
     _movement_engine = None
     _telescope_engine = None
     _position_output = False
+    _START_HEIGHT = 600
+    _TELESCOPE_HEIGHT = 150
 
     def __init__(self, movement_engine, telescope_engine):
         self._xposition_sensor = XPositionSensor()
         self._movement_engine = movement_engine
         self._telescope_engine = telescope_engine
 
-    def _calculate_x(self):
+    def calculate_x(self):
         return (self._xposition_sensor.get_position() + self._movement_engine.get_x()) / 2
 
-    def _calculate_z(self):
-        return 100
+    def calculate_z(self, x):
+        return (math.tan(0.141897) * x) + self._START_HEIGHT - self._TELESCOPE_HEIGHT - self._telescope_engine.get_z()
 
-    def _show_position(self):
+    def show_position(self):
         while self._position_output:
-            x = self._calculate_x()
-            z = self._calculate_z()
-            PosistionCommand.set_current_position(x, z)
+            x = self.calculate_x()
+            z = self.calculate_z(x)
+            # TODO: Output the current position.
             time.sleep(1)
-
 
     def start_position_output(self):
         self._position_output = True
-        self._show_position()
+        self.show_position()
 
     def stop_position_output(self):
         self._position_output = False
