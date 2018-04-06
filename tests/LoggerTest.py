4a7fca2cea270e72517bec81f818114c2ff50871
wtjerry <wtjerry123@gmail.com>
2018-04-06 10:41:17 +0200

    fixed broken tests

--- tests/LoggerTest.py
+++ tests/LoggerTest.py
@@ -6,19 +6,15 @@ from controlling.Logger import Logger
 class LoggerTest(unittest.TestCase):
     def test_info(self):
         connection_handler_mock = MagicMock()
-        queue_mock = MagicMock()
-        connection_handler_mock.queue = queue_mock
         logger = Logger(connection_handler_mock)
         test_data = "test data"
 
         logger.info(test_data)
 
-        queue_mock.append.assert_called_once_with(test_data)
+        connection_handler_mock.enqueue_message.assert_called_once_with(test_data)
 
     def test_major_step(self):
         connection_handler_mock = MagicMock()
-        queue_mock = MagicMock()
-        connection_handler_mock.queue = queue_mock
         logger = Logger(connection_handler_mock)
         test_data = "test data"
 
@@ -29,4 +25,4 @@ class LoggerTest(unittest.TestCase):
             + test_data + "\n" \
             + "----------------------------" + "\n" \
             + "                            " + "\n"
-        queue_mock.append.assert_called_once_with(expected_data)
+        connection_handler_mock.enqueue_message.assert_called_once_with(expected_data)
