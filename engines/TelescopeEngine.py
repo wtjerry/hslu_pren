5adece2366fb279a6920550fc66774d0f45f62a4
Cyrille <cyrille.ulmi@stud.hslu.ch>
2018-04-06 10:02:19 +0200

    Changed telescope engine to new specifications

--- engines/TelescopeEngine.py
+++ engines/TelescopeEngine.py
@@ -1,6 +1,6 @@
 class TelescopeEngine(object):
     _UP_COMMAND = "HSup"
-    _DOWN_COMMAND = "HSdown"
+    _DOWN_COMMAND = "HSdn"
     _GET_Z_COMMAND = "HSgetz"
     _communication = None
 
@@ -8,11 +8,19 @@ class TelescopeEngine(object):
         self._communication = communication
 
     def up(self, mm):
-        self._communication.execute(self._UP_COMMAND + mm)
+        mm_formatted = self._get_formatted_mm(mm)
+        self._communication.execute(self._UP_COMMAND + mm_formatted)
 
     def down(self, mm):
-        self._communication.execute(self._DOWN_COMMAND + mm)
+        mm_formatted = self._get_formatted_mm(mm)
+        self._communication.execute(self._DOWN_COMMAND + mm_formatted)
 
     def get_z(self):
         z = self._communication.execute(self._GET_Z_COMMAND)
         return int(z)
+
+    def _get_formatted_mm(self, mm):
+        mm_string = str(mm)
+        while len(mm_string) < 3:
+            mm_string = "0" + mm_string
+        return mm_string
